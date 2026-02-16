01a6f095664d14483aa4d28e0190b58d880ae239
Christian Sadiki <christiantyty.sadiki@sms-group.com>
2026-02-16 09:13:56 +0100

    Expose report_service in package init

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,11 +1,9 @@
-from .data_service import data_service
-from .profile_generator import profile_generator
-from .prediction_service import prediction_service
-from .export_service import export_service
+from .report_service import report_service
 
 __all__ = [
     "data_service",
     "profile_generator",
     "prediction_service",
-    "export_service"
+    "export_service",
+    "report_service"
 ]
